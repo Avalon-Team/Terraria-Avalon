8bb63cffd24006030cbdfde3e8c0ab15e5f32c28
PoroCYon <pietersluys2@gmail.com>
2014-06-09 18:44:08 +0200

    Ported GuiAccessorySlots

---
+++ Avalon/AccessorySlots.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using TAPI;
+using PoroCYon.MCT.UI.Interface;
+using PoroCYon.MCT.UI.Interface.Controls;
+
+namespace Avalon
+{
+    class AccessorySlots : CustomUI
+    {
+        static AccessorySlots instance;
+
+        public class Items
+        {
+            public Item this[int index]
+            {
+                get
+                {
+                    return instance.slots[index].ContainedItem;
+                }
+                set
+                {
+                    instance.slots[index].ContainedItem = value;
+                }
+            }
+        }
+        internal static Items Slots
+        {
+            get;
+            private set;
+        }
+
+        ItemContainer[] slots = new ItemContainer[Mod.ExtraSlots];
+
+        internal AccessorySlots()
+            : base()
+        {
+            instance = this;
+
+            for (int i = 0; i < slots.Length; i++)
+                slots[i] = new ItemContainer(new Item(), Mod.Instance, i, "Avalon:ExtraAccSlot") { InventoryBackTextureNum = 3 };
+
+            Slots = new Items();
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            for (int i = 0; i < slots.Length; i++)
+                AddControl(slots[i]);
+        }
+    }
+}
