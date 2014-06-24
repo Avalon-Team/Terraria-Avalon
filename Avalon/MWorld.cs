30649e830a7406a3cb0d76879674c92dc978adbf
PoroCYon <pietersluys2@gmail.com>
2014-06-24 13:04:04 +0200

    More MWorld stuff

--- Avalon/MWorld.cs
+++ Avalon/MWorld.cs
@@ -17,9 +17,7 @@ namespace Avalon
     {
         const int spawnSpaceX = 3, spawnSpaceY = 3;
 
-        static bool needsToLoadRecipes = true;
-        static bool addedWings = false;   // no idea
-        static bool scanned = false; // no idea
+        static bool needsToLoadRecipes = true, addedWings = false, scanned = false; // no idea
 
         internal static bool oldNight = false;
 
@@ -49,9 +47,9 @@ namespace Avalon
         /// </summary>
         public static int HallowAltarsBroken = 0;
         /// <summary>
-        /// How many times something something wraith invasion something.
+        /// How many times a wraith in the Wraith Invasion was killed.
         /// </summary>
-        public static int WraithInvasionCount = 0;
+        public static int WraithsDowned = 0;
 
         /// <summary>
         /// Gets the ID of the Golden Wings texture.
@@ -62,7 +60,7 @@ namespace Avalon
             private set;
         }
 
-        static int grassCounter = 0, jungleEx = 0;
+        static int grassCounter = 0, jungleEx = 0, gCount = 0;
 
         /// <summary>
         /// Gets the rectangle for the Tropics biome.
@@ -140,7 +138,56 @@ namespace Avalon
         {
             base.PostUpdate();
 
+            if (!Mod.IsInSuperHardmode && !UltraOblivionDowned)
+                return;
 
+            NPC n, n2;
+
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                n = Main.npc[i];
+
+                if (n.type == 94 && n.active && Mod.IsInSuperHardmode)
+                    for (int j = 0; j < Main.npc.Length; j++)
+                    {
+                        n2 = Main.npc[j];
+
+                        if (n2.type == Defs.npcs["Hallowor"].type && n2.active && n.Hitbox.Intersects(n2.Hitbox))
+                        {
+                            Make3x3Circle((int)n.position.X / 16, (int)n.position.Y / 16, TileDef.type["Oblivion Ore"]);
+
+                            n.active = n2.active = false;
+                            n.NPCLoot();
+                            Main.PlaySound(4, (int)n.position.X, (int)n.position.Y, n.soundKilled);
+                            n2.NPCLoot();
+                            Main.PlaySound(4, (int)n2.position.X, (int)n2.position.Y, n2.soundKilled);
+
+                            NetHelper.SendText("Dark and light have been obliterated...", new Color(135, 78, 0));
+                        }
+                    }
+
+                if (n.type == Defs.npcs["Guardian Corruptor"].type && n.active && UltraOblivionDowned)
+                    for (int j = 0; j < Main.npc.Length; j++)
+                    {
+                        n2 = Main.npc[j];
+
+                        if (n2.type == Defs.npcs["Aegis Hallowor"].type && n2.active && n.Hitbox.Intersects(n2.Hitbox))
+                        {
+                            Make3x3Circle((int)n.position.X / 16, (int)n.position.Y / 16, TileDef.type["Berserker Ore"]);
+
+                            n.active = n2.active = false;
+                            n.NPCLoot();
+                            Main.PlaySound(4, (int)n.position.X, (int)n.position.Y, n.soundKilled);
+                            n2.NPCLoot();
+                            Main.PlaySound(4, (int)n2.position.X, (int)n2.position.Y, n2.soundKilled);
+
+                            NetHelper.SendText("Dark and light have been annihilated...", new Color(135, 78, 0));
+                        }
+                    }
+            }
+
+            if (WraithsDowned >= 200)
+                Mod.Wraiths.Stop();
         }
 
         /// <summary>
@@ -155,9 +202,32 @@ namespace Avalon
         }
 
         /// <summary>
-        /// Called when the world is loaded.
+        /// Writes binary data to a world save file. Called when the world is closed.
         /// </summary>
-        /// <param name="bb"></param>
+        /// <param name="bb">The buffer containing the binary data.</param>
+        public override void Save(BinBuffer bb)
+        {
+            base.Save(bb);
+
+            bb.WriteX(
+                Mod.IsInSuperHardmode,
+                UltraOblivionDowned,
+                SpawnedBerserkerOre,
+                scanned);
+
+            bb.WriteX(
+                CatarystDownedCount,
+                HallowAltarsBroken,
+                ArmageddonCount,
+                EverIceCount,
+                WraithsDowned);
+
+            bb.WriteX(TropicsRect.X, TropicsRect.Y, TropicsRect.Width, TropicsRect.Height);
+        }
+        /// <summary>
+        /// Loads binary data from a world save file. Called when the world is loaded.
+        /// </summary>
+        /// <param name="bb">The buffer containing the binary data.</param>
         public override void Load(BinBuffer bb)
         {
             base.Load(bb);
@@ -171,6 +241,19 @@ namespace Avalon
                 for (int j = 0; j < Mod.ExtraSlots; j++)
                     accessories[i][j] = new Item();
             }
+
+            Mod.IsInSuperHardmode = bb.ReadBool();
+            UltraOblivionDowned = bb.ReadBool();
+            SpawnedBerserkerOre = bb.ReadBool();
+            scanned = bb.ReadBool();
+
+            CatarystDownedCount = bb.ReadInt();
+            HallowAltarsBroken = bb.ReadInt();
+            ArmageddonCount = bb.ReadInt();
+            EverIceCount = bb.ReadInt();
+            WraithsDowned = bb.ReadInt();
+
+            TropicsRect = new Rectangle(bb.ReadInt(), bb.ReadInt(), bb.ReadInt(), bb.ReadInt());
         }
 
         /// <summary>
@@ -210,6 +293,40 @@ namespace Avalon
         public static bool CheckForBosses()
         {
             return Main.npc.Count(n => n.boss) > 0; // LINQ++. again.
+        }
+
+        /// <summary>
+        /// Creates a 3x3 circle of tiles.
+        /// </summary>
+        /// <param name="x">The centre of the circle's X coordinate.</param>
+        /// <param name="y">The centre of the circle's Y coordinate.</param>
+        /// <param name="type">The type of the tiles.</param>
+        public static void Make3x3Circle(int x, int y, ushort type)
+        {
+            for (int xoff = x - 1; xoff <= x + 1; xoff++)
+                for (int yoff = y - 1; yoff <= y + 1; yoff++)
+                {
+                    Main.tile[xoff, yoff].active(true);
+                    Main.tile[xoff, yoff].type = type;
+                    
+                    WorldGen.SquareTileFrame(xoff, yoff);
+                }
+
+            Main.tile[x - 2, y].active(true);
+            Main.tile[x + 2, y].active(true);
+            Main.tile[x, y - 2].active(true);
+            Main.tile[x, y + 2].active(true);
+
+            Main.tile[x - 2, y].type
+                = Main.tile[x + 2, y].type
+                = Main.tile[x, y - 2].type
+                = Main.tile[x, y + 2].type
+                    = type;
+
+            WorldGen.SquareTileFrame(x - 2, y);
+            WorldGen.SquareTileFrame(x + 2, y);
+            WorldGen.SquareTileFrame(x, y - 2);
+            WorldGen.SquareTileFrame(x, y + 2);
         }
     }
 }
