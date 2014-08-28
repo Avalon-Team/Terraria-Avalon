3697b0760fead73724283b20bb9a876aa10080f9
PoroCYon <pietersluys2@gmail.com>
2014-08-28 20:34:07 +0200

    Added Meteor Masher

---
+++ Avalon/Item/Weapons/Flails/Meteor Masher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using TAPI;
+using Avalon.API.Items;
+
+namespace Avalon.Items.Weapons.Flails
+{
+    /// <summary>
+    /// The Meteor Masher.
+    /// </summary>
+    [ChainTexture("Meteor Chain.png", ReplaceFlailChain = true)]
+    public sealed class MeteorMasher : ModItem
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="MeteorMasher" /> class.
+        /// </summary>
+        /// <param name="base">The mod that owns this item.</param>
+        /// <param name="i">The <see cref="Item" /> to attach the <see cref="ModItem" /> to.</param>
+        public MeteorMasher(ModBase @base, Item i)
+            : base(@base, i)
+        {
+
+        }
+    }
+}
