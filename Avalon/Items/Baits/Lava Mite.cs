00c308d03f8770f9a800e23be169bf654d4a189e
PoroCYon <pietersluys2@gmail.com>
2014-10-07 21:38:18 +0200

    Unfinished lava mite

---
+++ Avalon/Items/Baits/Lava Mite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using TAPI;
+
+namespace Avalon.Items.Baits
+{
+	/// <summary>
+	/// The Lava Mite.
+	/// </summary>
+	public sealed class LavaMite : ModItem
+	{
+		/// <summary>
+		/// <!-- I don't feel like writing XmlDoc right now -->
+		/// </summary>
+		/// <param name="player"></param>
+		/// <returns></returns>
+		public override bool CanUse(Player player)
+		{
+			NPC.NewNPC(Main.mouseX + (int)Main.screenPosition.X, Main.mouseY + (int)Main.screenPosition.Y, NPCDef.byName["Avalon:Lava Mite"].type);
+
+			return true;
+		}
+	}
+}
---
+++ Avalon/NPCs/Baits/Lava Mite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using TAPI;
+
+namespace Avalon.NPCs.Baits
+{
+	/// <summary>
+	/// The Lava Mite.
+	/// </summary>
+	public sealed class LavaMite : ModNPC
+	{
+		/// <summary>
+		/// Called after <see cref="NPC.AI" /> is called.
+		/// </summary>
+		public override void PostAI()
+		{
+			base.PostAI();
+
+			// float in lava
+			if (npc.lavaWet)
+				npc.position.Y -= 0.025f;
+		}
+
+		/// <summary>
+		/// Checks whether the spawning mechanism should spawn an <see cref="NPC" /> or not.
+		/// </summary>
+		/// <param name="x">The X position of the spawn place (in tiles).</param>
+		/// <param name="y">The X position of the spawn place (in tiles).</param>
+		/// <param name="type">The type of the <see cref="NPC" /> that might spawn.</param>
+		/// <param name="spawnedOn">The <see cref="Player" /> the <see cref="NPC" /> might spawn on.</param>
+		/// <returns>true if the <see cref="NPC" /> should spawn; otherwise, false.</returns>
+		public override bool CanSpawn(int x, int y, int type, Player spawnedOn)
+		{
+			return Biome.Biomes["Hell"].Check(spawnedOn);// && Main.rand.Next(-1) == 0; // need a rarity value
+		}
+	}
+}
