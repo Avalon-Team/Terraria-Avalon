Avalon-Team/Terraria-Avalon

commit 45443e0371dc32ba80dcbb86606363d20261a915

>1 mole of code. Fml.

Avalon/Avalon.csproj
@@ -73,7 +73,17 @@
   <ItemGroup>
     <Compile Include="API\ExtendedSpawning.cs" />
     <Compile Include="API\StarterSets\StarterSet.cs" />
+    <Compile Include="Buffs\Brain Follower.cs" />
+    <Compile Include="Buffs\Freeze.cs" />
+    <Compile Include="Items\Accessories\Bag of Blood.cs" />
+    <Compile Include="Items\Accessories\Bag of Fire.cs" />
+    <Compile Include="Items\Accessories\Bag of Shadow.cs" />
+    <Compile Include="Items\Accessories\Shiver Heart.cs" />
     <Compile Include="Items\Baits\Lava Mite.cs" />
+    <Compile Include="Items\DevItems\Bullseye55\Bullseye55 Breastplate.cs" />
+    <Compile Include="Items\DevItems\Bullseye55\Bullseye55 Dev Pickaxe.cs" />
+    <Compile Include="Items\DevItems\Bullseye55\Bullseye55 Dev Warhammer.cs" />
+    <Compile Include="Items\DevItems\Bullseye55\Hard Swap.cs" />
     <Compile Include="Items\Mystical Tomes\Adventures and Mishaps.cs" />
     <Compile Include="Items\Mystical Tomes\Tome of the River Spirit.cs" />
     <Compile Include="Items\Mystical Tomes\Tome of Distance.cs" />
@@ -87,11 +97,48 @@
     <Compile Include="Items\Mystical Tomes\Creator%27s Tome.cs" />
     <Compile Include="Items\Mystical Tomes\Christmas Tome.cs" />
     <Compile Include="Items\Mystical Tomes\Burning Desire.cs" />
+    <Compile Include="Items\Armour\Dark Matter\Dark Matter Chest.cs" />
+    <Compile Include="Items\Potions\Brain in a Bottle.cs" />
+    <Compile Include="Items\Potions\Medicine Bottle.cs" />
+    <Compile Include="Items\Weapons\Ranged\Bows\Vertebrae Bow.cs" />
+    <Compile Include="Items\Weapons\Ranged\Cannons\Hand Cannon.cs" />
+    <Compile Include="Items\Weapons\Ranged\Cannons\Vertebrae Cannon.cs" />
+    <Compile Include="Items\Weapons\Ranged\Guns\Cursed Flamethrower.cs" />
+    <Compile Include="Items\Weapons\Ranged\Guns\Gatlineater.cs" />
+    <Compile Include="Items\Weapons\Ranged\Guns\Ichorthrower.cs" />
     <Compile Include="MNet.cs" />
     <Compile Include="MPlayer_AccEffects.cs" />
     <Compile Include="MUI.cs" />
     <Compile Include="NPCs\Baits\Lava Mite.cs" />
+    <Compile Include="NPCs\Normal\Armoured Crimslime.cs" />
+    <Compile Include="NPCs\Normal\Black Lancer.cs" />
+    <Compile Include="NPCs\Normal\Brain Follower.cs" />
+    <Compile Include="NPCs\Normal\Bunny Bones.cs" />
+    <Compile Include="NPCs\Normal\Crawleye.cs" />
+    <Compile Include="NPCs\Normal\Crimper.cs" />
+    <Compile Include="NPCs\Normal\Cursed Flamer.cs" />
+    <Compile Include="NPCs\Normal\Dark Matter Spearworm Body.cs" />
+    <Compile Include="NPCs\Normal\Dark Matter Spearworm Head.cs" />
+    <Compile Include="NPCs\Normal\Dark Matter Spearworm Tail.cs" />
+    <Compile Include="NPCs\Normal\Normal Barrel.cs" />
+    <Compile Include="NPCs\Normal\Verteworm Body.cs" />
+    <Compile Include="NPCs\Normal\Verteworm Head.cs" />
+    <Compile Include="NPCs\Normal\Verteworm Tail.cs" />
     <Compile Include="NPCs\VanillaDrop.cs" />
+    <Compile Include="Projectiles\FromPlayer\Bullets\Binge Eater.cs" />
+    <Compile Include="Projectiles\FromPlayer\Spears\Black Lance.cs" />
+    <Compile Include="Projectiles\FromPlayer\Bullets\Blood Ball.cs" />
+    <Compile Include="Projectiles\FromPlayer\Bullets\Cursed Flamethrower.cs" />
+    <Compile Include="Projectiles\FromPlayer\Spells\Freeze Bolt.cs" />
+    <Compile Include="Projectiles\FromPlayer\Explosions\Ichor Explosion.cs" />
+    <Compile Include="Projectiles\FromPlayer\Ichor Glob.cs" />
+    <Compile Include="Projectiles\FromPlayer\Throwables\Ichor Grenade.cs" />
+    <Compile Include="Projectiles\FromPlayer\Bullets\Ichorthrower.cs" />
+    <Compile Include="Projectiles\FromPlayer\Bullets\Soul Eater.cs" />
+    <Compile Include="Projectiles\FromPlayer\Throwables\Sticky Spike Ball.cs" />
+    <Compile Include="Projectiles\FromPlayer\Throwables\Throwing Paladin%27s Hammer.cs" />
+    <Compile Include="Projectiles\FromPlayer\Arrows\Vertebrae Rivet.cs" />
+    <Compile Include="Projectiles\FromPlayer\Bullets\Zapper Bullet.cs" />
     <Compile Include="UI\AccessorySlotLayer.cs" />
     <Compile Include="API\Biomes\Attributes.cs" />
     <Compile Include="API\Items\Attributes.cs" />
@@ -143,8 +190,8 @@
     <Content Include="Buffs\Brain Follower.png" />
     <Content Include="Buffs\Freeze.png" />
     <Content Include="Buffs\Shadows.png" />
-    <Content Include="Gores\Armored Crimslime Gore 1.png" />
-    <Content Include="Gores\Armored Crimslime Gore 2.png" />
+    <Content Include="Gores\Armoured Crimslime Gore 1.png" />
+    <Content Include="Gores\Armoured Crimslime Gore 2.png" />
     <Content Include="Gores\Black Lancer Gore 1.png" />
     <Content Include="Gores\Black Lancer Gore 2.png" />
     <Content Include="Gores\Black Lancer Gore 3.png" />
@@ -250,10 +297,10 @@
     <Content Include="Items\Other\Shadow Mirror.png" />
     <Content Include="Items\Placeables\Heartstone Brick.png" />
     <Content Include="Items\Placeables\Vertebrae Block.png" />
-    <Content Include="Items\Placeables\Vertebrae Bookshelf.png" />
-    <Content Include="Items\Placeables\Vertebrae Chest.png" />
+    <Content Include="Items\Placeables\Furniture\Vertebrae Bookshelf.png" />
+    <Content Include="Items\Placeables\Furniture\Vertebrae Chest.png" />
     <Content Include="Items\Placeables\Vertebrae Platform.png" />
-    <Content Include="Items\Placeables\Vertebrae Tomb.png" />
+    <Content Include="Items\Placeables\Furniture\Vertebrae Tomb.png" />
     <Content Include="Items\Placeables\Walls\Dark Matter Brick Wall.png" />
     <Content Include="Items\Placeables\Walls\Dark Matter Wood Wall.png" />
     <Content Include="Items\Placeables\Walls\Heartstone Brick Wall.png" />
@@ -287,16 +334,16 @@
     <Content Include="Items\Weapons\Melee\Throwable\Sticky Spike Ball.png" />
     <Content Include="Items\Weapons\Melee\Throwable\Throwing Paladin%27s Hammer.png" />
     <Content Include="Items\Weapons\Ranged\Guns\Binge Eater.png" />
-    <Content Include="Items\Weapons\Ranged\Guns\Cannons\Hand Cannon.png" />
-    <Content Include="Items\Weapons\Ranged\Guns\Cannons\Vertebrae Cannon.png" />
+    <Content Include="Items\Weapons\Ranged\Cannons\Hand Cannon.png" />
+    <Content Include="Items\Weapons\Ranged\Cannons\Vertebrae Cannon.png" />
     <Content Include="Items\Weapons\Ranged\Bows\Vertebrae Bow.png" />
-    <Content Include="Items\Weapons\Ranged\Guns\Cannons\Ichor Gatling Cannon.png" />
+    <Content Include="Items\Weapons\Ranged\Cannons\Ichor Gatling Cannon.png" />
     <Content Include="Items\Weapons\Ranged\Guns\Cursed Flamethrower.png" />
     <Content Include="Items\Weapons\Ranged\Guns\Gatlineater.png" />
     <Content Include="Items\Weapons\Ranged\Guns\Ichorthrower.png" />
     <Content Include="Items\Weapons\Ranged\Guns\Soul Eater.png" />
     <Content Include="NPCs\Baits\Lava Mite.png" />
-    <Content Include="NPCs\Normal\Armored Crimslime.png" />
+    <Content Include="NPCs\Normal\Armoured Crimslime.png" />
     <Content Include="NPCs\Normal\Black Lancer.png" />
     <Content Include="NPCs\Normal\Brain Follower.png" />
     <Content Include="NPCs\Normal\Bunny Bones.png" />
@@ -439,7 +486,7 @@
     <Folder Include="Items\Weapons\Melee\Broadswords\" />
     <Folder Include="Items\Weapons\Melee\Shortswords\" />
     <Folder Include="Items\Weapons\Minion\" />
-    <Folder Include="Items\Weapons\Ranged\Bows\Crossbows\" />
+    <Folder Include="Items\Weapons\Ranged\Crossbows\" />
     <Folder Include="NPCs\Bosses\" />
     <Folder Include="Projectiles\FromNPC\" />
   </ItemGroup>
@@ -597,6 +644,8 @@
     <Content Include="Items\Potions\Shadow Potion.json" />
   </ItemGroup>
   <ItemGroup>
+    <None Include="Buffs\Brain Follower.json" />
+    <None Include="Buffs\Freeze.json" />
     <None Include="Buffs\Shadows.json" />
     <Content Include="Items\Weapons\Magic\Spells\Impulse Strike.json" />
     <Content Include="Projectiles\FromPlayer\Spells\Impulse Bolt.json" />
@@ -633,8 +682,86 @@
     <EmbeddedResource Include="ModOptions.json" />
     <Content Include="Items\Baits\Lava Mite.json" />
     <Content Include="Items\Materials\Heartstone.json" />
+    <None Include="Items\Accessories\Bag of Blood.json" />
+    <None Include="Items\Accessories\Bag of Fire.json" />
+    <None Include="Items\Accessories\Bag of Shadow.json" />
+    <None Include="Items\Accessories\Shiver Heart.json" />
+    <None Include="Items\Ammo\Zapper Bullet.json" />
+    <None Include="Items\Armour\Dark Matter\Dark Matter Chest.json" />
+    <None Include="Items\Armour\Dark Matter\Dark Matter Clinger.json" />
+    <None Include="Items\Armour\Dark Matter\Dark Matter Legs.json" />
+    <None Include="Items\Armour\Mushroom\MushroomBreastplate.json" />
+    <None Include="Items\Armour\Mushroom\MushroomHelmet.json" />
+    <None Include="Items\Armour\Mushroom\MushroomLeggings.json" />
+    <None Include="Items\DevItems\Bullseye55\Bullseye55 Breastplate.json" />
+    <None Include="Items\DevItems\Bullseye55\Bullseye55 Dev Pickaxe.json" />
+    <None Include="Items\DevItems\Bullseye55\Bullseye55 Dev Warhammer.json" />
+    <None Include="Items\DevItems\Bullseye55\Bullseye55 Helmet.json" />
+    <None Include="Items\DevItems\Bullseye55\Bullseye55 Leggings.json" />
+    <None Include="Items\DevItems\Bullseye55\Hard Swap.json" />
+    <None Include="Items\Placeables\Dark Matter Soil.json" />
+    <None Include="Items\Placeables\Flesh Tendril.json" />
+    <None Include="Items\Placeables\Furniture\Vertebrae Bookshelf.json" />
+    <None Include="Items\Placeables\Heartstone Brick.json" />
+    <None Include="Items\Placeables\Vertebrae Platform.json" />
+    <None Include="Items\Placeables\Walls\Heartstone Brick Wall.json" />
+    <None Include="Items\Placeables\Walls\Vertebrae Wall.json" />
+    <None Include="Items\Potions\Brain in a Bottle.json" />
+    <None Include="Items\Potions\Medicine Bottle.json" />
+    <None Include="Items\Weapons\Magic\Spells\Freeze Bolt.json" />
+    <None Include="Items\Weapons\Magic\Staffs\Vertebrae Staff.json" />
+    <None Include="Items\Weapons\Melee\Throwable\Ichor Grenade.json" />
+    <None Include="Items\Weapons\Melee\Throwable\Spike Ball.json" />
+    <None Include="Items\Weapons\Melee\Throwable\Sticky Spike Ball.json" />
+    <None Include="Items\Weapons\Ranged\Bows\Vertebrae Bow.json" />
+    <None Include="Items\Weapons\Ranged\Guns\Binge Eater.json" />
+    <None Include="Items\Weapons\Ranged\Cannons\Hand Cannon.json" />
+    <None Include="Items\Weapons\Ranged\Cannons\Ichor Gatling Cannon.json" />
+    <None Include="Items\Weapons\Ranged\Cannons\Vertebrae Cannon.json" />
+    <None Include="Items\Weapons\Ranged\Guns\Cursed Flamethrower.json" />
+    <None Include="Items\Weapons\Ranged\Guns\Gatlineater.json" />
+    <None Include="Items\Weapons\Ranged\Guns\Ichorthrower.json" />
+    <None Include="Items\Weapons\Ranged\Guns\Soul Eater.json" />
     <None Include="NPCs\Baits\Lava Mite.json" />
+    <None Include="NPCs\Normal\Armoured Crimslime.json" />
+    <None Include="NPCs\Normal\Black Lancer.json" />
+    <None Include="NPCs\Normal\Brain Follower.json" />
+    <None Include="NPCs\Normal\Bunny Bones.json" />
+    <None Include="NPCs\Normal\Crawleye.json" />
+    <None Include="NPCs\Normal\Crimper.json" />
+    <None Include="NPCs\Normal\Cursed Flamer.json" />
+    <None Include="NPCs\Normal\Dark Matter Spearworm Body.json" />
+    <None Include="NPCs\Normal\Dark Matter Spearworm Head.json" />
+    <None Include="NPCs\Normal\Dark Matter Spearworm Tail.json" />
+    <None Include="NPCs\Normal\Normal Barrel.json" />
+    <None Include="NPCs\Normal\Verteworm Body.json" />
+    <None Include="NPCs\Normal\Verteworm Head.json" />
+    <None Include="NPCs\Normal\Verteworm Tail.json" />
+    <None Include="Projectiles\FromPlayer\Bullets\Binge Eater.json" />
+    <None Include="Projectiles\FromPlayer\Spears\Black Lance.json" />
+    <None Include="Projectiles\FromPlayer\Bullets\Blood Ball.json" />
+    <None Include="Projectiles\FromPlayer\Bullets\Cursed Flamethrower.json" />
+    <None Include="Projectiles\FromPlayer\Spells\Freeze Bolt.json" />
+    <None Include="Projectiles\FromPlayer\Explosions\Ichor Explosion.json" />
+    <None Include="Projectiles\FromPlayer\Ichor Glob.json" />
+    <None Include="Projectiles\FromPlayer\Throwables\Ichor Grenade.json" />
+    <None Include="Projectiles\FromPlayer\Bullets\Ichorthrower.json" />
+    <None Include="Projectiles\FromPlayer\Bullets\Soul Eater.json" />
+    <None Include="Projectiles\FromPlayer\Throwables\Spike Ball.json" />
+    <None Include="Projectiles\FromPlayer\Throwables\Sticky Spike Ball.json" />
+    <None Include="Projectiles\FromPlayer\Throwables\Throwing Paladin%27s Hammer.json" />
+    <None Include="Projectiles\FromPlayer\Arrows\Vertebrae Rivet.json" />
+    <None Include="Projectiles\FromPlayer\Bullets\Zapper Bullet.json" />
+    <None Include="Tiles\Blocks\Dark Matter Soil.json" />
+    <None Include="Tiles\Blocks\Flesh Tendril.json" />
+    <None Include="Tiles\Blocks\Heartstone Brick.json" />
     <None Include="Tiles\Ores\Heartstone.json" />
+    <None Include="Items\Weapons\Melee\Throwable\Throwing Paladin%27s Hammer.json" />
+    <None Include="Tiles\Blocks\Vertebrae Block.json" />
+    <None Include="Tiles\Furniture\Vertebrae Bookshelf.json" />
+    <None Include="Tiles\Blocks\Vertebrae Platform.json" />
+    <None Include="Walls\Heartstone Brick Wall.json" />
+    <None Include="Walls\Vertebrae Wall.json" />
   </ItemGroup>
   <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
   <PropertyGroup>

Avalon/Items/Weapons/Ranged/Guns/Cursed Flamethrower.cs
@@ -1,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using TAPI;
+
+namespace Avalon.Items.Weapons.Ranged.Guns
+{
+    /// <summary>
+    /// The Cursed Flamethrower.
+    /// </summary>
+    public sealed class CursedFlamethrower : ModItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public override bool ConsumeAmmo(Player p)
+        {
+            return Main.rand.Next(6) == 1;
+        }
+    }
+}

Avalon/Items/Weapons/Ranged/Guns/Ichorthrower.cs
@@ -1,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using TAPI;
+
+namespace Avalon.Weapons.Ranged.Guns
+{
+    /// <summary>
+    /// The Ichorthrower.
+    /// </summary>
+    public class Ichorthrower : ModItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public override bool ConsumeAmmo(Player p)
+        {
+            return Main.rand.Next(6) == 0;
+        }
+    }
+}

Avalon/Projectiles/FromPlayer/Throwables/Sticky Spike Ball.cs
@@ -1,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using TAPI;
+
+namespace Avalon.Projectiles.FromPlayer.Throwables
+{
+    /// <summary>
+    /// The Sticky Spike Ball.
+    /// </summary>
+    public sealed class StickySpikeBall : ModProjectile
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="velocityChange"></param>
+        /// <returns></returns>
+        public override bool OnTileCollide(ref Vector2 velocityChange) 
+        {
+            projectile.velocity = Vector2.Zero;
+
+            return false;
+        }
+    }
+}

Avalon/Projectiles/FromPlayer/Bullets/Cursed Flamethrower.cs
@@ -1,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TAPI;
+
+namespace Avalon.Projectiles.FromPlayer.Bullets
+{
+    /// <summary>
+    /// The Cursed Flamethrower.
+    /// </summary>
+    public class CursedFlamethrower : ModProjectile
+    {          
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void AI()
+        {
+            Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, 75, 0, 0, 100, new Color(), 2.8f)].noGravity = false;
+
+            projectile.rotation = (float)Math.Atan2(projectile.velocity.Y, projectile.velocity.X);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="npc"></param>
+        /// <param name="hitDir"></param>
+        /// <param name="damage"></param>
+        /// <param name="knockback"></param>
+        /// <param name="crit"></param>
+        /// <param name="critMult"></param>
+        public override void DamageNPC(NPC npc, int hitDir, ref int damage, ref float knockback, ref bool crit, ref float critMult)
+        {
+             npc.AddBuff(39, 600, true);
+        }
+    }
+}

Avalon/Projectiles/FromPlayer/Bullets/Ichorthrower.cs
@@ -1,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TAPI;
+
+namespace Avalon.Projectiles.FromPlayer.Bullets
+{
+    /// <summary>
+    /// The Ichorthrower.
+    /// </summary>
+    public sealed class Ichorthrower : ModProjectile
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void AI()
+        {
+            Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, 162, 0, 0, 100, new Color(), 2.8f)].noGravity = false;
+
+            projectile.rotation = (float)Math.Atan2(projectile.velocity.Y, projectile.velocity.X);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="npc"></param>
+        /// <param name="hitDir"></param>
+        /// <param name="damage"></param>
+        /// <param name="knockback"></param>
+        /// <param name="crit"></param>
+        /// <param name="critMult"></param>
+        public override void DamageNPC(NPC npc, int hitDir, ref int damage, ref float knockback, ref bool crit, ref float critMult)
+        {
+             npc.AddBuff(69, 600, true);
+        }
+    }
+}

commit d363c14f43d48fc25fcdac7de413b7b144394383

Fixed bugs that were introduced in 45443e0371 , and fixed #5.

Avalon/Avalon.csproj
@@ -108,7 +108,7 @@
     <Compile Include="Items\Weapons\Ranged\Guns\Ichorthrower.cs" />
     <Compile Include="MNet.cs" />
     <Compile Include="MPlayer_AccEffects.cs" />
-    <Compile Include="MUI.cs" />
+    <Compile Include="UI\MUI.cs" />
     <Compile Include="NPCs\Baits\Lava Mite.cs" />
     <Compile Include="NPCs\Normal\Armoured Crimslime.cs" />
     <Compile Include="NPCs\Normal\Black Lancer.cs" />

Avalon/Items/Weapons/Ranged/Guns/Cursed Flamethrower.cs
@@ -16,6 +16,15 @@ namespace Avalon.Items.Weapons.Ranged.Guns
         /// </summary>
         /// <param name="p"></param>
         /// <returns></returns>
+        public override bool CanUse(Player p)
+        {
+            return p.inventory.Any(i => !i.IsBlank() && i.type == 522);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public override bool ConsumeAmmo(Player p)
         {
             return Main.rand.Next(6) == 1;

Avalon/Items/Weapons/Ranged/Guns/Ichorthrower.cs
@@ -16,6 +16,15 @@ namespace Avalon.Weapons.Ranged.Guns
         /// </summary>
         /// <param name="p"></param>
         /// <returns></returns>
+        public override bool CanUse(Player p)
+        {
+            return p.inventory.Any(i => !i.IsBlank() && i.type == 1332);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public override bool ConsumeAmmo(Player p)
         {
             return Main.rand.Next(6) == 0;

Avalon/Projectiles/FromPlayer/Throwables/Sticky Spike Ball.cs
@@ -18,7 +18,7 @@ namespace Avalon.Projectiles.FromPlayer.Throwables
         /// <returns></returns>
         public override bool OnTileCollide(ref Vector2 velocityChange) 
         {
-            projectile.velocity = Vector2.Zero;
+            velocityChange = projectile.velocity = Vector2.Zero;
 
             return false;
         }

Avalon/UI/MUI.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using TAPI;
-using Avalon.UI;
 
-namespace Avalon
+namespace Avalon.UI
 {
     /// <summary>
     /// The <see cref="ModInterface" /> class of the Avalon mod.

Avalon/UI/TomeSlotLayer.cs
@@ -41,8 +41,11 @@ namespace Avalon.UI
         /// <param name="sb"></param>
         protected override void OnDraw(SpriteBatch sb)
         {
-            slot.Update(  );
-			slot.Draw  (sb);
+            if (Main.playerInventory)
+            {
+                slot.Update(  );
+			    slot.Draw  (sb);
+            }
         }
     }
 }